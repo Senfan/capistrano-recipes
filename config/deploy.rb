c555e2c48e6cde44007b0109651dfdf5ae473410
teddy-hoo <hulingchuan@hotmail.com>
2014-08-14 13:16:59 +0800

    hard code github info

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,11 +5,6 @@ require 'json'
 file = File.read('./config/server.json')
 data = JSON.parse(file)
 
-puts data
-puts data['server']
-puts data['server'][0]
-puts data['server'][0]['host']
-
 host = data['server'][0]['host']
 user = data['server'][0]['username']
 
@@ -39,16 +34,6 @@ set :bundle_path, -> { shared_path.join('bundle') }
 set :bundle_without, %w{development test}.join(' ')
 set :bundle_flags, '--no-deployment'
 set :bundle_env_variables, {}
-
-#set stage
-#set :stage, 'production'
-
-#set rbenv
-#set :rbenv_type, :user # or :system, depends on your rbenv setup
-#set :rbenv_ruby, "#{fetch(:ruby_version)}"
-#set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{#fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-#set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-#set :rbenv_roles, :all # default value
 
 namespace :env do
   desc "environment setup"
@@ -104,25 +89,24 @@ namespace :github do
   desc "configure github environment"
   task :setup do
     on roles(:web) do
-      ask(:option, "do you want to config github?(Y)")
-      if("#{fetch(:option)}" == "Y")
-        ask(:email, "input email address: ")
-        file = "~/.ssh/id_rsa"
-        public_file = "#{file}.pub"
-        execute "git config --global core.editor 'vi'"
-        execute "git config --global user.email '#{fetch(:email)}'"
-        if capture("if [ -f #{file} ]; then echo 'true'; fi") == ''
-          execute "ssh-keygen -q -t rsa -C '#{fetch(:email)}' -N '' -f '#{file}' "
-        end
-        key = capture "cat #{public_file}"
-        ask(:username, "input github username: ")
-        ask(:password, "input github password: ")
-        github = Github.new( login: username, password: password )
-        github.users.keys.create( title: "capistrano generated", key: key )
+      #ask(:email, "input email address: ")
+      email = "hulingchuan@hotmail.com"
+      file = "~/.ssh/id_rsa"
+      public_file = "#{file}.pub"
+      execute "git config --global user.email '#{email}'"
+      if capture("if [ -f #{file} ]; then echo 'true'; fi") == ''
+        execute "ssh-keygen -q -t rsa -C '#{email}' -N '' -f '~/ssh/id_rsa' "
       end
+      key = capture("cat #{public_file}")
+      #ask(:username, "input github username: ")
+      #ask(:password, "input github password: ")
+      username="teddy-hoo"
+      password="wd521ywn"
+      github = Github.new( login: "#{username}", password: "#{password}" )
+      github.users.keys.create( title: "capistrano generated", key: key )
     end
   end
-
+ 
 end
 
 
