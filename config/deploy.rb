defbb23acc664b7fa2a6fdc523e38ba34dd8c0db
teddy-hoo <hulingchuan@hotmail.com>
2014-08-15 10:29:29 +0800

    add github ssh config

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,7 @@ require 'json'
 file = File.read('./config/server.json')
 data = JSON.parse(file)
 
-host = data['server'][0]['host']
-user = data['server'][0]['username']
+user = data['server']['staging']['username']
 
 git_repo=data["git_repo"]
 ruby_version="2.1.2"
@@ -21,10 +20,6 @@ set :scm, :git
 set :repo_url, "#{git_repo}"
 set :deploy_to, "/home/#{user}/webapp"
 set :pty, true
-
-#set :stage, :staging
-#set :branch, "master"
-#server "#{host}", roles: [:web], user: "#{user}"
 
 set :bundle_roles, :all
 set :bundle_servers, -> { release_roles(fetch(:bundle_roles)) }
@@ -89,20 +84,22 @@ namespace :github do
   desc "configure github environment"
   task :setup do
     on roles(:web) do
-      # ask(:email, "input email address: ")
-      email = "hulingchuan@hotmail.com"
+      ask(:email, "input email address: ")
+      execute "printf 'Host github.com \\n\\t User git" +
+              " \\n\\t Hostname ssh.github.com" +
+              " \\n\\t PreferredAuthentications publickey" +
+              " \\n\\t IdentityFile ~/.ssh/id_rsa" +
+              " \\n\\t Port 443\\n' > ~/.ssh/config"
       file = "~/.ssh/id_rsa"
       public_file = "#{file}.pub"
-      execute "git config --global user.email '#{email}'"
+      execute "git config --global user.email '#{fetch(:email)}'"
       if capture("if [ -f #{file} ]; then echo 'true'; fi") == ''
-        execute "ssh-keygen -q -t rsa -C '#{email}' -N '' -f '~/ssh/id_rsa' "
+        execute "ssh-keygen -q -t rsa -C '#{fetch(:email)}' -N '' -f '~/ssh/id_rsa' "
       end
       key = capture("cat #{public_file}")
-      # ask(:username, "input github username: ")
-      # ask(:password, "input github password: ")
-      username="teddy-hoo"
-      password="wd521ywn"
-      github = Github.new( login: "#{username}", password: "#{password}" )
+      ask(:username, "input github username: ")
+      ask(:password, "input github password: ")
+      github = Github.new( login: "#{fetch(:username)}", password: "#{fetch(:password)}" )
       github.users.keys.create( title: "capistrano generated", key: key )
     end
   end
