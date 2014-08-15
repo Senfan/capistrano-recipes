030d4be328a695bb3108651d6b9be90e19e73cb6
teddy-hoo <hulingchuan@hotmail.com>
2014-08-15 09:39:53 +0800

    add staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,3 +3,4 @@
 # Supports bulk-adding hosts to roles, the primary server in each group
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
+server "10.110.178.13", roles: [:web], user: "newhire"
