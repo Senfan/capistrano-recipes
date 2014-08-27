bdac58f73d5a40688a5d8e20692462b5ddbdb877
newhire <hulingchuan@hotmail.c>
2014-08-27 02:44:18 -0700

    enable testing stage

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,59 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-
-ruby_version="2.1.2"
+load "config/env_setup.rb"
+load "config/ruby_setup.rb"
+load "config/bundle_install.rb"
 
 set :application, 'newhire'
-set :user, "devops"
-set :scm, :git
-set :repo_url, "git@github.com/vmwarechina/newhire"
-set :deploy_to, "/home/#{user}/webapp"
-set :pty, true
+set :repo_url, 'git@github.com:/vmwarechina/newhire'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+#for test, will be removed later
+namespace :test do
+  task :test do
+    on roles(:nginx) do
+      execute "echo 'for test'"
+    end
+  end
+end
 
 namespace :deploy do
 
-  #before "github:setup", "env:setup"
-  #before "ruby:setup", "github:setup"
-  #before "deploy", "ruby:setup"
-  #after "deploy", "github:setup"
-  after 'deploy', 'bundle:install'
-  desc 'start appliction'
-  task :start do
-    on roles(:web) do
-      execute "$ruby #{release_path}/server.rb"
-    end
-  end
-
   desc 'Restart application'
   task :restart do
-    on roles(:web) do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
