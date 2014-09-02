bd625344040dc5aaeb6ae15e53e76c0202f501c1
rogerluo410 <rogerluo410@gmail.com>
2014-09-02 10:08:11 +0800

    change variable

--- config/loadinfo.rb
+++ config/loadinfo.rb
@@ -3,14 +3,14 @@ require './vm_access'
 #read server list from server.json
 #json structure refer to server.json
 server_info = File.read('./config/server.json')
-servers     = JSON.parse(server_info)
+Servers     = JSON.parse(server_info)
 
 #weiqi provide the following two method
 vma = VmAccess.new('10.110.178.12','root','vmware','Datacenter','ubuntu14.04_template')
-vma.remove_vms(servers) # remove existing vms
-vma.create_vms(servers) # create new vms and write info to servers object
+vma.remove_vms(Servers) # remove existing vms
+vma.create_vms(Servers) # create new vms and write info to servers object
 
 #write new server info to file
 File.open('./config/server.json', 'w') do |f|
-	f.write(servers.to_json)
+	f.write(Servers.to_json)
 end
