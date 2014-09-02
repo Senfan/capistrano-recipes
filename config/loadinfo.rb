e25f4dd043010ac489e40168e671f02d80f38c78
rogerluo410 <rogerluo410@gmail.com>
2014-09-02 15:10:58 +0800

    Add enhancement for datasote selection and VM deployment in parallel

--- config/loadinfo.rb
+++ config/loadinfo.rb
@@ -6,7 +6,9 @@ server_info = File.read('./config/server.json')
 Servers     = JSON.parse(server_info)
 
 #weiqi provide the following two method
-vma = VmAccess.new('10.110.178.12','root','vmware','Datacenter','ubuntu14.04_template')
+#parameters expression:
+#VmAccess.new (host,username,password,datacenter_name,cluster_name,source_template_name) 
+vma = VmAccess.new('10.110.178.12','root','vmware','Datacenter','cluster','ubuntu14.04_template')
 vma.remove_vms(Servers) # remove existing vms
 vma.create_vms(Servers) # create new vms and write info to servers object
 
