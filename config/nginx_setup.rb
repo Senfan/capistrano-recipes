d1be067c31795476e4c3d0db66412a8d3a7b9bf5
GarfieldLinux <garfieldlinux@gmail.com>
2014-09-04 16:38:38 +0800

    nginx static files & index.html

--- config/nginx_setup.rb
+++ config/nginx_setup.rb
@@ -3,8 +3,8 @@
 
 require_relative 'loadinfo'
 
-sinatraweb1 = Servers['servers']['staging']['nginx'][0]['ip']
-sinatraweb2 = Servers['servers']['staging']['nginx'][1]['ip']
+sinatraweb1 = Servers['servers']['staging']['sinatra'][0]['ip']
+sinatraweb2 = Servers['servers']['staging']['sinatra'][1]['ip']
 
 namespace :nginx do
    desc "install nginx"
@@ -17,7 +17,10 @@ namespace :nginx do
                  "events {\\n worker_connections 768;\\n  }\\n " +
                  "http {\\n upstream webservers {\\n  server #{sinatraweb1} ;\\n " +
                  "server #{sinatraweb2} ;\\n }\\n server {\\n listen       80; \\n" +
-                 "server_name  webservers;\\n location / {\\n " +
+                 "location / { \\n index   index.html; \\n } \\n" +
+                 "location ~ .*\\.(gif|jpg|jpeg|png|bmp|swf|js|html|htm|css)\$ { \\n" +
+                 "root  /home/devops/webapp/current/; \\n }\\n" +
+                 "server_name  webservers;\\n location /api {\\n " +
                  "proxy_pass  http://webservers;\\n proxy_redirect off;\\n" +
                  "}\\n }\\n sendfile on;\\n tcp_nopush on;\\n tcp_nodelay on;\\n" +
                  "keepalive_timeout 65;\\n types_hash_max_size 2048;\\n " +
