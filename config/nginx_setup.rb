bf97f072c5cf57ac30f8fc124ce5659b170c5333
teddy-hoo <hulingchuan@hotmail.com>
2014-09-10 09:37:22 +0800

    add production config

--- config/nginx_setup.rb
+++ config/nginx_setup.rb
@@ -3,23 +3,35 @@
 
 require_relative 'loadinfo'
 
-sinatraweb1 = Servers['servers']['staging']['sinatra'][0]['ip']
-sinatraweb2 = Servers['servers']['staging']['sinatra'][1]['ip']
-
 namespace :nginx do
    desc "install nginx"
    task :setup do
       on roles(:nginx) do
-         execute "sudo apt-get -y install nginx"
-         execute "sudo /etc/init.d/nginx stop"
+         if "#{deploy_to}".include? "production"
+             root_path = "production"
+             sinatraweb1 = Servers['servers']['production']['sinatra'][0]['ip']
+             sinatraweb2 = Servers['servers']['production']['sinatra'][1]['ip']
+             execute "sudo /etc/init.d/nginx stop"
+         elsif "#{deploy_to}".include? "staging"
+             root_path = "staging"
+             sinatraweb1 = Servers['servers']['staging']['sinatra'][0]['ip']
+             sinatraweb2 = Servers['servers']['staging']['sinatra'][1]['ip']
+             execute "sudo apt-get -y install nginx"
+             execute "sudo /etc/init.d/nginx stop"
+         else
+             root_path = "webapp"
+             execute "sudo apt-get -y install nginx"
+             execute "sudo /etc/init.d/nginx stop"
+         end
+         
          execute "sudo bash -c \"echo -e 'user www-data; \\n " +
                  "worker_processes 4; \\n pid /run/nginx.pid;\\n " +
                  "events {\\n worker_connections 768;\\n  }\\n " +
                  "http {\\n upstream webservers {\\n  server #{sinatraweb1}:9292 ;\\n " +
                  "server #{sinatraweb2}:9292 ;\\n }\\n server {\\n listen       80; \\n" +
-                 "location =/ { \\n root  /home/devops/webapp/current/; \\n index   index.html; \\n } \\n" +
+                 "location =/ { \\n root  /home/devops/#{root_path}/current/; \\n index   index.html; \\n } \\n" +
                  "location ~ .*\\.(gif|jpg|jpeg|png|bmp|swf|js|html|htm|css)\$ { \\n" +
-                 "root  /home/devops/webapp/current/; \\n }\\n" +
+                 "root  /home/devops/#{root_path}/current/; \\n }\\n" +
                  "server_name  webservers;\\n location / {\\n " +
                  "proxy_pass  http://webservers/;\\n " +
                  "}\\n }\\n sendfile on;\\n tcp_nopush on;\\n tcp_nodelay on;\\n" +
