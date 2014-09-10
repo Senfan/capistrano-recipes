f1dea1dca0fd08e4f95dc0b88df49d4fe22cc4e9
teddy-hoo <hulingchuan@hotmail.com>
2014-09-10 08:59:54 +0800

    remove extra comments

--- config/loadinfo.rb
+++ config/loadinfo.rb
@@ -1,20 +1,14 @@
 require 'json'
 require_relative './vm_access'
-#read server list from server.json
-#json structure refer to server.json
+
 server_info = File.read('./config/server.json')
 Servers     = JSON.parse(server_info)
 
-#weiqi provide the following two method
-#parameters expression:
-#VmAccess.new (host_name,username,password,datacenter_name,cluster_name,source_template_name) 
 vma = VmAccess.new('10.110.178.12','root','vmware','Datacenter','cluster','ubuntu14.04_template')
-#remove_vms(Servers) # remove existing vms
-#create_vms(Servers) # create new vms and write info to servers object
-vma.remove_vms(Servers) # remove existing vms
-vma.create_vms(Servers) # create new vms and write info to servers object
 
-#write new server info to file
+vma.remove_vms(Servers)
+vma.create_vms(Servers)
+
 File.open('./config/server.json', 'w') do |f|
 	f.write(Servers.to_json)
 end
