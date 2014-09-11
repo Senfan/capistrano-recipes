f21301f3d30790a13fa227d8a63c5b038622a8f2
teddy-hoo <hulingchuan@hotmail.com>
2014-09-11 10:44:30 +0800

    add config backup task

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,8 @@ namespace :deploy do
 
   before "postgresql:setup", "ruby:setup"
   before "nginx:setup", "postgresql:setup"
-  before "deploy", "nginx:setup"
+  before "config:backup", "nginx:setup"
+  before "deploy", "config:backup"
 
   task :dbsetup do
     on roles(:sinatra) do
@@ -32,6 +33,10 @@ namespace :deploy do
           execute "cd #{release_path} && sed -i '8s/.*/  port: 389/' config/config.yml"
           execute "cd #{release_path} && sed -i '9s/.*/  base: dc=vmware,dc=com/' config/config.yml"
         end
+      elsif "#{deploy_to}".include? "production"
+        execute "cd #{deploy_to}; cd ..; mv config.tar.gz #{deploy_to}"
+        execute "cd #{deploy_to}; rm -r config/"
+        execute "cd #{deploy_to}; tar -zxvf config.tar.gz'"      
       end
     end
   end
